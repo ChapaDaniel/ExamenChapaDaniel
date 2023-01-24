3133de79db78d4f2aea037b5b6dd0fd6444cdc66
Daniel Chapa <ChapaDaniel479@gmail.com>
2023-01-24 12:09:38 -0500

    Examen

---
+++ ExamenChapa/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+
+const routes: Routes = [
+  {
+    path:'',component:C1Component
+  },
+  {
+    path:'segunda',component: C2Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
---
+++ ExamenChapa/src/app/c1/c1.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c1',
+  templateUrl: './c1.component.html',
+  styleUrls: ['./c1.component.css']
+})
+export class C1Component {
+any Text: Nombres
+
+
+
+}
+
+
+void Nombres {
+  this.nombres=('nbs')
+}
+
+
+void Apellidos {
+  this.Apellidos=('apd')
+}
+
+
+void Dirección {
+  this.Dirección=('drc')
+}
+
+
+void Celular {
+  this.celular=('cll')
+}
+
+
+void Edad {
+  this.Edad=('edad')
+}
+
